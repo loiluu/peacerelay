a0ebdbc3ca8eed1b0ebb674faad1ffbd983088a6
naterush <narush@wharton.upenn.edu>
2017-07-14 15:45:57 -0400

    add basic outline of functions

--- contracts/PeaceRelay.sol
+++ contracts/PeaceRelay.sol
@@ -8,11 +8,16 @@ contract PeaceRelay {
   using RLP for bytes;
 
 	mapping (bytes32 => BlockHeader) blocks;
+	mapping (bytes32 => Transaction) transactions;
 
 	struct BlockHeader {
       uint      prevBlockHash;// 0
 			bytes32		txRoot;				// 4
   }
+
+	struct Transaction {
+		//data
+	}
 
 	//For now, just assume all blocks are good + valid.
 	//In the future, will use SmartPool's verification.
@@ -22,6 +27,19 @@ contract PeaceRelay {
 		blocks[blockHash] = header;
 	}
 
+	function verifyTransaction(bytes rlpProof, bytes rlpPath, bytes rlpTransaction, bytes32 blockHash) {
+		BlockHeader memory header = blocks[blockHash];
+
+		if (!checkProof(rlpProof, rlpPath, rlpTransaction)) {
+			throw;
+		}
+
+		bytes32 txHash = sha3(rlpTransaction);
+		transactions[txHash] = parseTransaction(rlpTransaction);
+	}
+
+
+	// HELPER FUNCTIONS
 
 	function parseBlockHeader(bytes rlpHeader) constant internal returns(BlockHeader) {
   	BlockHeader memory header;
@@ -37,5 +55,31 @@ contract PeaceRelay {
 		  idx++;
   	}
 		return header;
+  }
+
+	function checkProof(bytes rlpProof, bytes rlpPath, bytes rlpTransaction) returns (bool) {
+		return true;
+	}
+
+
+	function parseTransaction(bytes rlpTransaction) constant internal returns(Transaction) {
+  	Transaction memory transaction;
+  	var it = rlpTransaction.toRLPItem().iterator();
+
+		uint idx;
+  	while(it.hasNext() && idx < 5) {
+			/*
+
+			HAVE TO FIGURE OUT INDEXS OF STUFF IN TRANSACTION
+
+			if (idx == 0) {
+				header.prevBlockHash = it.next().toUint();
+			} else if (idx == 4) {
+				header.txRoot = bytes32(it.next().toUint());
+			}
+		  idx++;
+			*/
+  	}
+		return transaction;
   }
 }
